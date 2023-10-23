20ac5c61700177545eabfb9bbb4c8d74c5334f69
redkorr <redkor910.97@gmail.com>
2023-10-23 21:45:28 +0200

    feat: added errors to products endpoint

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import productsRouter from './products/index.js';
+import categoriesRouter from './categories/index.js';
 
 const router = Router();
 router.use('/products', productsRouter);
+router.use('/categories', categoriesRouter);
 
 export default router;
--- server/routes/products/index.ts
+++ server/routes/products/index.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
-import Cloth from '../../models/Cloth.js';
+import Product from '../../models/Products.js';
+import { MongooseError } from 'mongoose';
 
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const data = await Cloth.find({});
-  console.log(data);
-  res.send(data);
+  try {
+    const data = await Product.find().populate('category');
+    console.log(data);
+    res.status(200).json(data);
+  } catch (error) {
+    if (error instanceof MongooseError) {
+      res.status(500).json(error);
+    }
+  }
 });
 
 export default router;
