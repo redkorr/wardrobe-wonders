a348b9b740d4909639c36aa63b25723aa2301e2c
redkorr <redkor910.97@gmail.com>
2023-10-23 21:22:41 +0200

    feat: created NavBar component

--- onlineshop/src/components/NavBar.tsx
+++ onlineshop/src/components/NavBar.tsx
@@ -1,18 +1,44 @@
+import useCategories from '@/hooks/useCategories';
+import LINKS from '@/utils/linkPaths';
 import { UserButton, useAuth, useSignIn } from '@clerk/clerk-react';
-import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'lucide-react';
+import { Link, NavLink, useParams } from 'react-router-dom';
+import { Category } from 'types';
 
 const Navbar = () => {
   const { isSignedIn } = useAuth();
   const { isLoaded } = useSignIn();
+  const { sex } = useParams();
+
+  const categories: Array<Category> | undefined = useCategories();
 
   return (
-    <div className="">
-      {!isSignedIn && isLoaded && <Link to="/checkout/login">ab</Link>}
-      <UserButton
-        showName
-        afterSignOutUrl="/"
-      />
-    </div>
+    <nav className="z-50 fixed top-0 flex items-center w-full p-6 border border-red-600 justify-between">
+      <div className="text-3xl font-bold">Wardrobe Wonders</div>
+      <div className="text-2xl font-semibold">
+        {sex?.toLocaleLowerCase() === 'his' ? (
+          <Link to={LINKS.productsHer}>Her</Link>
+        ) : (
+          <Link to={LINKS.productsHis}>His</Link>
+        )}
+      </div>
+      <div>
+        {categories?.map((category) => (
+          <NavLink to={`${LINKS.products}/${sex}/${category.name}`}>{category.name}</NavLink>
+        ))}
+      </div>
+      <div className="flex gap-4">
+        {!isSignedIn && isLoaded ? (
+          <Link to={LINKS.checkoutLogin}>Sign In</Link>
+        ) : (
+          <UserButton
+            showName
+            afterSignOutUrl="/"
+          />
+        )}
+        <ShoppingCart />
+      </div>
+    </nav>
   );
 };
 
