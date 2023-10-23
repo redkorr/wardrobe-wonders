c679449368db3f0b35bab9f666c9cef91d9f2dd9
redkorr <redkor910.97@gmail.com>
2023-10-23 21:50:39 +0200

    feat: created links lookup object

---
+++ onlineshop/src/utils/linkPaths.ts
@@ -0,0 +1,11 @@
+const LINKS = {
+  checkoutLogin: '/checkout/login',
+  productsHis: '/products/his',
+  productsHer: '/products/her',
+  clothes: '/clothes',
+  shoes: '/shoes',
+  accessories: '/accessories',
+  products: '/products'
+} as const;
+
+export default LINKS;
