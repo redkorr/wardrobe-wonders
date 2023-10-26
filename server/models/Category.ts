b49226debb3543b397c83f7377889266fdbe619f
redkorr <redkor910.97@gmail.com>
2023-10-26 22:13:53 +0200

    feat: updated schema and model of category and products

--- server/models/Category.ts
+++ server/models/Category.ts
@@ -2,12 +2,18 @@ import mongoose, { Schema, model } from 'mongoose';
 
 export interface Category {
   name: string;
+  types: mongoose.Types.ObjectId[];
 }
 
 const categorySchema = new Schema<Category>({
   name: {
     type: Schema.Types.String,
     unique: true,
+    required: true,
+  },
+  types: {
+    type: [Schema.Types.ObjectId],
+    ref: 'ProductType',
     required: true,
   },
 });
