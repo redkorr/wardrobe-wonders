a020d73f4301ceda5164bf669a4dde6e6a721a13
redkorr <redkor910.97@gmail.com>
2023-10-26 22:15:01 +0200

    feat: updated endpoint

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import productsRouter from './products/index.js';
 import categoriesRouter from './categories/index.js';
+import productTypeRouter from './productTypes/index.js';
 
 const router = Router();
 router.use('/products', productsRouter);
 router.use('/categories', categoriesRouter);
+router.use('/types', productTypeRouter);
 
 export default router;
