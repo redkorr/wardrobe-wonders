18b0cb60a9ac0e576d3a55a8a50c5a33f2f740e4
redkorr <redkor910.97@gmail.com>
2023-10-26 22:12:33 +0200

    feat: updated mock data and categories

--- server/mockdata/categories.ts
+++ server/mockdata/categories.ts
@@ -1,13 +1,28 @@
+import { Types } from 'mongoose';
 import { type Category } from '../models/Category.js';
 
 export const categories: Array<Category> = [
   {
     name: 'cloth',
+    types: [
+      new Types.ObjectId('653a64ff0de8e3dc995d7691'),
+      new Types.ObjectId('653a64ff0de8e3dc995d7692'),
+      new Types.ObjectId('653a64ff0de8e3dc995d7693'),
+      new Types.ObjectId('653a64ff0de8e3dc995d7694'),
+    ],
   },
   {
     name: 'shoes',
+    types: [
+      new Types.ObjectId('653a64ff0de8e3dc995d7695'),
+      new Types.ObjectId('653a64ff0de8e3dc995d7696'),
+    ],
   },
   {
     name: 'accessories',
+    types: [
+      new Types.ObjectId('653a64ff0de8e3dc995d7697'),
+      new Types.ObjectId('653a64ff0de8e3dc995d7698'),
+    ],
   },
 ];
