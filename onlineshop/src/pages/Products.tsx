5563baa811005af20e5e1515979cfa6ee19dbddf
redkorr <redkor910.97@gmail.com>
2023-10-26 14:46:00 +0200

    feat: added basic products display in Products page

--- onlineshop/src/pages/Products.tsx
+++ onlineshop/src/pages/Products.tsx
@@ -1,12 +1,28 @@
 import { NavBar } from '@/components';
+import useProducts from '@/hooks/useProducts';
 import { useParams } from 'react-router-dom';
 
 const Products = () => {
   const { sex, category } = useParams();
+
+  const products = useProducts(sex, category);
+
   return (
-    <div>
+    <div className="pt-24">
       <NavBar />
-      {sex},{category}
+      <div className="flex">
+        <div className="w-1/6">
+          <h2>{category}</h2>
+        </div>
+        <div className="w-5/6">
+          {products?.map((product) => (
+            <div key={product.product_id}>
+              <img src={`/${product.category.name}/${product.type}/${product.images[0]}`} />
+              <h2>{product.name}</h2>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
