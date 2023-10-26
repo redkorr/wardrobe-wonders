813eb385dcac1e509aa59ad4ee3d44e6ef8dfc07
redkorr <redkor910.97@gmail.com>
2023-10-26 22:12:52 +0200

    feat: created mock product types

---
+++ server/mockdata/productTypes.ts
@@ -0,0 +1,36 @@
+import { type ProductType } from '../models/ProductType.js';
+
+export const productTypes: Array<ProductType> = [
+  {
+    name: 'hoodie',
+    display_name: 'hoodie',
+  },
+  {
+    name: 'shirt',
+    display_name: 'shirt',
+  },
+  {
+    name: 't-shirt',
+    display_name: 't-shirt',
+  },
+  {
+    name: 'pants',
+    display_name: 'pants',
+  },
+  {
+    name: 'sneakers',
+    display_name: 'sneakers',
+  },
+  {
+    name: 'flip-flops',
+    display_name: 'flip flops',
+  },
+  {
+    name: 'hats',
+    display_name: 'hats',
+  },
+  {
+    name: 'belts',
+    display_name: 'belts',
+  },
+];
