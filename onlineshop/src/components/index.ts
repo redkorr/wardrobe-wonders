d932cb0737e92b97a297aa17c51ac332dccf1cff
redkorr <redkor910.97@gmail.com>
2023-11-01 18:59:06 +0100

    feat: created ProductsHeader component

--- onlineshop/src/components/index.ts
+++ onlineshop/src/components/index.ts
@@ -9,3 +9,5 @@ export { default as TypeItem } from './Type/TypeItem';
 export { default as FiltersSection } from './FiltersSection';
 
 export { default as Breadcrumbs } from './Breadcrumbs';
+
+export { default as ProductsHeader } from './ProductsHeader';
