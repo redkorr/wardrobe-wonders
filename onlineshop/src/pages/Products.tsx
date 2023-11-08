235871d04acf9cf044123065d7bfbb1caaf54bb0
redkorr <redkor910.97@gmail.com>
2023-11-08 13:46:55 +0100

    feat: updated Products page

--- onlineshop/src/pages/Products.tsx
+++ onlineshop/src/pages/Products.tsx
@@ -1,26 +1,55 @@
-import { NavBar } from '@/components';
+import { FiltersSection, NavBar } from '@/components';
+import { CategoriesAccordion } from '@/components';
 import useProducts from '@/hooks/useProducts';
-import { useParams } from 'react-router-dom';
+import undraw_void from '@/assets/undraw_void.svg';
+import { useParams, useSearchParams } from 'react-router-dom';
+import { Product } from 'types';
 
 const Products = () => {
-  const { sex, category } = useParams();
+  const { sex, category, type } = useParams();
 
-  const products = useProducts(sex, category);
+  const searchParams = useSearchParams();
 
+  const products: Array<Product> | undefined = useProducts(sex, category, type);
   return (
-    <div className="pt-24">
+    <div className="pt-24 w-full">
       <NavBar />
-      <div className="flex">
-        <div className="w-1/6">
-          <h2>{category}</h2>
-        </div>
-        <div className="w-5/6">
-          {products?.map((product) => (
-            <div key={product.product_id}>
-              <img src={`/${product.category.name}/${product.type}/${product.images[0]}`} />
-              <h2>{product.name}</h2>
+      <div className="flex flex-row ">
+        <CategoriesAccordion
+          sex={sex}
+          activeCategory={category}
+        />
+        <div className="flex flex-col px-10">
+          <FiltersSection
+            numberOfProducts={products?.length}
+            sex={sex}
+            searchParams={searchParams}
+          />
+          {products?.length === 0 && (
+            <div className="flex w-full justify-center items-center">
+              <img
+                src={undraw_void}
+                alt="No products"
+                className=" w-80 h-80"
+              />
             </div>
-          ))}
+          )}
+
+          <div className="flex flex-wrap gap-14">
+            {products?.map((product) => (
+              <div
+                key={product.product_id}
+                className="w-[360px]"
+              >
+                <img
+                  width={360}
+                  height={540}
+                  src={`/${product.category.name}/${product.type.display_name}/${product.images[0]}`}
+                />
+                <h2 className="truncate text-lg mt-3">{product.name}</h2>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
