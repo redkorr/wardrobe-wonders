5f4c83a49311bcfe461631d49f8a8b4f9a572a02
redkorr <redkor910.97@gmail.com>
2023-11-08 13:46:05 +0100

    feat: updated useProducts hook with type argument

--- onlineshop/src/hooks/useProducts.ts
+++ onlineshop/src/hooks/useProducts.ts
@@ -1,16 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Product } from '../../types';
 
-const useProducts = (sex: string | undefined, category: string | undefined) => {
+const useProducts = (sex: string | undefined, category: string | undefined, type: string | undefined) => {
   const [data, setData] = useState<Array<Product> | undefined>(undefined);
   const env = import.meta.env.NODE_ENV === 'production' ? `${import.meta.env.VITE_API_URL}` : 'http://localhost:5000';
-  const URL = `${env}/api/products/${sex}${category ? `/${category}` : ''}`;
+  const URL = `${env}/api/products/${sex}${category ? `/${category}` : ''}${type ? `/${type}` : ''}`;
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch(URL);
         const json = await response.json();
-        console.log(response);
 
         setData(json);
       } catch (error) {
