9c20e0670cb7622bebd9746a9c0a1f81f7440c19
redkorr <redkor910.97@gmail.com>
2023-11-15 13:10:58 +0100

    chore: deleted mock button

--- onlineshop/src/components/FilterDropDown.tsx
+++ onlineshop/src/components/FilterDropDown.tsx
@@ -17,12 +17,8 @@ const FilterDropDown = () => {
 
       {isListOpen && (
         <div className="z-10 relative bg-white w-60 mt-2 border border-b-0 border-l-0 border-r-0">
+          {}
           <FilterCheckbox />
-          <button className="flex items-center gap-1 m-1">
-            Blue
-            <div className="w-2 h-2 bg-blue-700"></div>
-          </button>
-          <button className="text-center w-56 bg-[#f1f1f1] py-2 m-2">Accept</button>
         </div>
       )}
     </div>
