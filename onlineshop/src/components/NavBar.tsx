3ecaac65eb76cc56bf19546d970af493005137ac
redkorr <redkor910.97@gmail.com>
2024-02-20 12:18:22 +0100

    style: changed navbar style

--- onlineshop/src/components/NavBar.tsx
+++ onlineshop/src/components/NavBar.tsx
@@ -13,9 +13,9 @@ const Navbar = () => {
   const categories: Array<Category> | undefined = useCategories();
 
   return (
-    <nav className="z-50 fixed top-0 flex items-center w-full p-6 justify-between bg-slate-800">
-      <div className="text-3xl font-bold">Wardrobe Wonders</div>
-      <div className="text-2xl font-semibold">
+    <nav className="z-50 fixed top-0 flex items-center w-full p-6 justify-between bg-slate-950">
+      <div className="text-3xl font-bold text-gray-300">Wardrobe Wonders</div>
+      <div className="text-2xl font-semibold text-gray-300">
         {sex?.toLocaleLowerCase() === 'his' && <NavLink to={LINKS.productsHer}>Her</NavLink>}
         {sex?.toLocaleLowerCase() === 'her' && <NavLink to={LINKS.productsHis}>His</NavLink>}
       </div>
@@ -23,15 +23,15 @@ const Navbar = () => {
         {categories?.map((category) => (
           <NavLink
             key={category.name}
-            style={({ isActive }) => (isActive ? { fontWeight: 700, color: 'black' } : {})}
-            className="px-4 text-lg font-semibold"
+            style={({ isActive }) => (isActive ? { fontWeight: 700, color: 'white' } : {})}
+            className="px-4 text-lg font-semibold text-gray-300"
             to={`${LINKS.products}/${sex}/${category.name}`}
           >
             {category.name}
           </NavLink>
         ))}
       </div>
-      <div className="flex gap-8">
+      <div className="flex gap-8 text-gray-300">
         {!isSignedIn && isLoaded ? (
           <Link to={LINKS.checkoutLogin}>Sign In</Link>
         ) : (
