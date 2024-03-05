3f0c17a47b7c6462418d9496b19bd6cf38b38b13
redkorr <redkor910.97@gmail.com>
2024-03-05 13:06:53 +0100

    feat: added functionality to ProductInfo buttons

--- onlineshop/src/components/ProductInfo.tsx
+++ onlineshop/src/components/ProductInfo.tsx
@@ -1,14 +1,22 @@
 import { ChevronDown, ChevronUp, Shirt, ShoppingCart, Truck, Undo2 } from 'lucide-react';
-import { useState } from 'react';
+import { Dispatch, useState } from 'react';
 import { Product } from 'types';
 
 interface ProductInfoParams {
   product: Product | undefined;
+  setDetailsInfoState: Dispatch<React.SetStateAction<number>>;
 }
 
-const ProductInfo = ({ product }: ProductInfoParams) => {
+const ProductInfo = ({ product, setDetailsInfoState }: ProductInfoParams) => {
   const [selectedValue, setSelectedValue] = useState<string>('M');
   const [isActive, setIsActive] = useState(false);
+  const scrollToDetails = (index: number) => {
+    const element = document.getElementById('DetailsInfoHeader');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      setDetailsInfoState(index);
+    }
+  };
   return (
     <div className="w-3/4 p-6 mt-6 mx-auto">
       <h1 className="text-3xl mb-10">{product?.name}</h1>
@@ -52,15 +60,24 @@ const ProductInfo = ({ product }: ProductInfoParams) => {
         <p className="m-auto">Add to cart</p>
       </button>
       <div className="flex justify-center flex-wrap gap-4">
-        <button className="flex items-center gap-3 m-3 text-lg">
+        <button
+          className="flex items-center gap-3 m-3 text-lg"
+          onClick={() => scrollToDetails(0)}
+        >
           <Shirt />
           Product details
         </button>
-        <button className="flex items-center gap-3 m-3 text-lg">
+        <button
+          className="flex items-center gap-3 m-3 text-lg"
+          onClick={() => scrollToDetails(1)}
+        >
           <Truck />
           Shipping in up to 30 days
         </button>
-        <button className="flex items-center gap-3 m-3 text-lg">
+        <button
+          className="flex items-center gap-3 m-3 text-lg"
+          onClick={() => scrollToDetails(2)}
+        >
           <Undo2 />
           30 days to return
         </button>
