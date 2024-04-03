a4b82baa1a6a4893772b6ff0e90afe2000e910b4
redkorr <redkor910.97@gmail.com>
2024-04-03 13:18:56 +0200

    feat: created discount model and schema

---
+++ server/models/Discount.ts
@@ -0,0 +1,20 @@
+import mongoose, { Schema, model } from 'mongoose';
+
+export interface Discount {
+  value: number;
+  code: string;
+}
+
+const discountSchema = new Schema<Discount>({
+  value: {
+    type: Schema.Types.Number,
+    required: true,
+  },
+  code: {
+    type: Schema.Types.String,
+    unique: true,
+    required: true,
+  },
+});
+
+export default model<Discount>('Discount', discountSchema, 'discount');
