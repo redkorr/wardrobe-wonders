06882f67374d90b362f691b9d14f95fda1f227f7
redkorr <redkor910.97@gmail.com>
2024-04-16 13:04:56 +0200

    feat: added countinue without registration option

--- onlineshop/src/pages/Login.tsx
+++ onlineshop/src/pages/Login.tsx
@@ -1,14 +1,25 @@
+import { NavBar } from '@/components';
 import { SignIn } from '@clerk/clerk-react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>
-      <SignIn
-        path="/checkout/login"
-        routing="path"
-        signUpUrl="/checkout/register"
-        redirectUrl="/"
-      />
+    <div className="h-full">
+      <NavBar />
+      <div className="flex flex-col items-center justify-center h-full gap-4">
+        <SignIn
+          path="/checkout/login"
+          routing="path"
+          signUpUrl="/checkout/register"
+          redirectUrl="/checkout-r"
+        />
+        <Link
+          to="/checkout"
+          className="underline"
+        >
+          Continue without registration
+        </Link>
+      </div>
     </div>
   );
 };
