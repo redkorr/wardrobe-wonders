446d4e8afae21ec46a45c3a1ccd9e419b7413fd7
redkorr <redkor910.97@gmail.com>
2024-05-10 10:14:52 +0200

    feat: updated useDiscount hook

--- onlineshop/src/hooks/useDiscount.ts
+++ onlineshop/src/hooks/useDiscount.ts
@@ -1,22 +1,26 @@
 import { useState } from 'react';
 import { Discount } from '../../types';
+import { useAppDispatch } from './useAppDispatch';
+import { updateDiscountValue } from '@/features/shoppingCartSlice';
 
 const useDiscount = () => {
   const [discount, setDiscount] = useState<Discount | undefined>(undefined);
+  const dispatch = useAppDispatch();
   const env = import.meta.env.NODE_ENV === 'production' ? `${import.meta.env.VITE_API_URL}` : 'http://localhost:5000';
 
-  const fetchDiscount = async (code: string) => {
+  const updateDiscount = async (code: string) => {
     try {
       const response = await fetch(`${env}/api/discount/${code}`);
       const json = await response.json();
 
       setDiscount(json);
+      dispatch(updateDiscountValue(json.value));
     } catch (error) {
       console.error(error);
     }
   };
 
-  return { fetchDiscount, discount };
+  return { updateDiscount, discount };
 };
 
 export default useDiscount;
