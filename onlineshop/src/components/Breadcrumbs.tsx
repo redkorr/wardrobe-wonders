7b950fab8046dedba254984c7aaaea9840258986
redkorr <redkor910.97@gmail.com>
2024-05-17 14:06:48 +0200

    fix: fixed breadcrumbs

--- onlineshop/src/components/Breadcrumbs.tsx
+++ onlineshop/src/components/Breadcrumbs.tsx
@@ -9,12 +9,7 @@ const Breadcrumbs = () => {
     <div className="flex">
       {pathNamePartsArray.map((pathParam, index) => (
         <React.Fragment key={index}>
-          <NavLink
-            style={index === pathNamePartsArray.length - 1 ? { fontWeight: 700, color: 'white' } : {}}
-            to={linkTo(index)}
-          >
-            {pathParam}
-          </NavLink>
+          <NavLink to={pathParam === 'products' ? '/' : linkTo(index)}>{pathParam}</NavLink>
           {index !== pathNamePartsArray.length - 1 && <span>&nbsp;&gt;&nbsp;</span>}
         </React.Fragment>
       ))}
