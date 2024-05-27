0eb745059f3439e748de6935c1188949bee7fc5e
redkorr <redkor910.97@gmail.com>
2024-05-27 12:11:15 +0200

    feat: created summary page

---
+++ onlineshop/src/pages/Summary.tsx
@@ -0,0 +1,19 @@
+const Summary = () => {
+  return (
+    <div>
+      <div>
+        <p>your order is confirmed</p>
+        <p> An email was sent to you at {}</p>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <th></th>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
