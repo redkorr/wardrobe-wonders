b6f5e2f7f90079367d26a764bb7bfedcdf5c701f
redkorr <redkor910.97@gmail.com>
2024-06-04 13:57:02 +0200

    feat: changed barckend order type

--- server/models/Order.ts
+++ server/models/Order.ts
@@ -12,12 +12,12 @@ const order_statuses = [
 export type OrderStatus = (typeof order_statuses)[number];
 
 export interface Order {
-  user_id: mongoose.Types.ObjectId;
-  payment_id: mongoose.Types.ObjectId;
+  user_id: string;
   total: number;
   date: Date;
   order_status: OrderStatus;
   order_items: Array<OrderItem> | null;
+  billing_address: BillingAddress;
 }
 
 export interface OrderItem {
@@ -25,8 +25,19 @@ export interface OrderItem {
   name: string;
   image_path: string;
   color_name: string;
+  price: number;
   size: string;
   quantity: number;
+}
+
+export interface BillingAddress {
+  first_name: string;
+  last_name: string;
+  address: string;
+  postal_code: string;
+  city: string;
+  phone_number: number;
+  email: string;
 }
 
 const orderItemSchema = new Schema<OrderItem>({
@@ -34,13 +45,23 @@ const orderItemSchema = new Schema<OrderItem>({
   name: { type: Schema.Types.String, required: true },
   image_path: { type: Schema.Types.String, required: true },
   color_name: { type: Schema.Types.String, required: true },
+  price: { type: Schema.Types.Number, required: true },
   size: { type: Schema.Types.String, required: true },
   quantity: { type: Schema.Types.Number, required: true },
 });
 
+const billingAddressSchema = new Schema<BillingAddress>({
+  first_name: { type: Schema.Types.String, required: true },
+  last_name: { type: Schema.Types.String, required: true },
+  address: { type: Schema.Types.String, required: true },
+  postal_code: { type: Schema.Types.String, required: true },
+  city: { type: Schema.Types.String, required: true },
+  phone_number: { type: Schema.Types.Number, required: true },
+  email: { type: Schema.Types.String, required: true },
+});
+
 const orderSchema = new Schema<Order>({
-  user_id: { type: Schema.Types.ObjectId, required: true },
-  payment_id: { type: Schema.Types.ObjectId, required: true },
+  user_id: { type: Schema.Types.String },
   total: { type: Schema.Types.Number, required: true },
   date: { type: Schema.Types.Date, required: true },
   order_status: { type: Schema.Types.String, required: true },
@@ -48,6 +69,11 @@ const orderSchema = new Schema<Order>({
     type: [Schema.Types.Mixed],
     of: orderItemSchema,
     default: () => null,
+    required: true,
+  },
+  billing_address: {
+    type: [Schema.Types.Mixed],
+    of: billingAddressSchema,
     required: true,
   },
 });
