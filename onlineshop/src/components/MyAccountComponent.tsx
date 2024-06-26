b1b79f7b5b9f6b86c28bc047a12b2f965ab31464
redkorr <redkor910.97@gmail.com>
2024-06-26 13:07:03 +0200

    feat: created MyAccountComponent

---
+++ onlineshop/src/components/MyAccountComponent.tsx
@@ -0,0 +1,11 @@
+import DeliveryAddressForm from './BillingAddressForm';
+
+const MyAccountComponent = () => {
+  return (
+    <div className="pt-24 w-full">
+      <DeliveryAddressForm />
+    </div>
+  );
+};
+
+export default MyAccountComponent;
