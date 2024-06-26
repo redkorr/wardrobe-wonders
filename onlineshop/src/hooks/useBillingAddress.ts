b732749b5fab88f5dca384d2c8c84764ea98a1b5
redkorr <redkor910.97@gmail.com>
2024-06-26 13:07:45 +0200

    feat: created hook useBillingAddress

---
+++ onlineshop/src/hooks/useBillingAddress.ts
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { BillingAddressData } from '../../types';
+
+const useBillingAddress = () => {
+  const [data, setData] = useState<BillingAddressData>();
+  const env = import.meta.env.NODE_ENV === 'production' ? `${import.meta.env.VITE_API_URL}` : 'http://localhost:5000';
+  const URL = `${env}/api/delivery-address`;
+  async function postBillingAddress(deliveryAddress: BillingAddressData) {
+    try {
+      await fetch(URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(deliveryAddress)
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getBillingAddresses(userId: string) {
+    try {
+      const response = await fetch(`${URL}/${userId}`);
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return { postBillingAddress, getBillingAddresses, data };
+};
+export default useBillingAddress;
