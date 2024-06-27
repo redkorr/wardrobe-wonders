1971504d8804440a04157b4d0ecc2c543632b96c
redkorr <redkor910.97@gmail.com>
2024-06-27 13:05:07 +0200

    feat: created UserPageNavbar component

---
+++ onlineshop/src/components/UserPageNavbar.tsx
@@ -0,0 +1,37 @@
+import { Dispatch } from 'react';
+
+interface UserPageNavbarProps {
+  setIndex: Dispatch<React.SetStateAction<number>>;
+  index: number;
+}
+
+const UserPageNavbar = ({ setIndex, index }: UserPageNavbarProps) => {
+  return (
+    <div className="flex gap-4">
+      <button
+        className="p-4"
+        style={
+          index === 0
+            ? { borderBottom: '1px solid', fontSize: '18px', fontWeight: '600' }
+            : { borderBottom: '0', fontSize: '16px', fontWeight: '400' }
+        }
+        onClick={() => setIndex(0)}
+      >
+        My Account
+      </button>
+      <button
+        className="p-4"
+        style={
+          index === 1
+            ? { borderBottom: '1px solid', fontSize: '18px', fontWeight: '600' }
+            : { borderBottom: '0', fontSize: '16px', fontWeight: '400' }
+        }
+        onClick={() => setIndex(1)}
+      >
+        My Orders
+      </button>
+    </div>
+  );
+};
+
+export default UserPageNavbar;
