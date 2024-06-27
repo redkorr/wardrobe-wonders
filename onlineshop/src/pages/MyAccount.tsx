725c382dccaf3118b99d3afcaf3998b4330d1cfd
redkorr <redkor910.97@gmail.com>
2024-06-27 13:05:34 +0200

    feat: updated MyAccount page

--- onlineshop/src/pages/MyAccount.tsx
+++ onlineshop/src/pages/MyAccount.tsx
@@ -1,12 +1,27 @@
 import { MyAccountComponent, MyOrders } from '@/components';
 import Navbar from '@/components/NavBar';
+import UserPageNavbar from '@/components/UserPageNavbar';
+import { useState } from 'react';
 
 const MyAccount = () => {
+  const [index, setIndex] = useState<number>(0);
+  const displayComponent = () => {
+    const components = [<MyAccountComponent />, <MyOrders />];
+    return components[index];
+  };
   return (
-    <div>
+    <div className="flex justify-center">
       <Navbar />
-      <MyAccountComponent />
-      <MyOrders />
+      <div className="mt-24 w-3/5">
+        <p className="text-3xl">Hello!</p>
+        <hr className="mt-3" />
+        <UserPageNavbar
+          setIndex={setIndex}
+          index={index}
+        />
+        <hr className="mb-3" />
+        {displayComponent()}
+      </div>
     </div>
   );
 };
