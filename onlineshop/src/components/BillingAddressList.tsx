1ea2ddd6376b331fa84e37b8ad4cc7697e4f42d2
redkorr <redkor910.97@gmail.com>
2024-06-28 12:58:55 +0200

    chore: renamed BillingAddresses component to BillingAddressList

---
+++ onlineshop/src/components/BillingAddressList.tsx
@@ -0,0 +1,48 @@
+import useBillingAddress from '@/hooks/useBillingAddress';
+import { useUser } from '@clerk/clerk-react';
+import { ChevronLeft, Pen } from 'lucide-react';
+import { Dispatch, useEffect } from 'react';
+import { BillingAddressData } from 'types';
+
+interface BillingAddressListProps {
+  setIndex: Dispatch<React.SetStateAction<number>>;
+  setBillingAddress: Dispatch<React.SetStateAction<BillingAddressData | undefined>>;
+}
+
+const BillingAddressList = ({ setIndex, setBillingAddress }: BillingAddressListProps) => {
+  const { data, getBillingAddressList } = useBillingAddress();
+  const { user } = useUser();
+  useEffect(() => {
+    if (user) {
+      getBillingAddressList(user.id);
+    }
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => setIndex(0)}>
+        <ChevronLeft />
+      </button>
+      <div className="p-5">
+        {data?.map((billingAddress) => (
+          <button
+            onClick={() => {
+              setBillingAddress(billingAddress);
+              setIndex(3);
+            }}
+            className="text-lg w-full border h-32 my-3 relative"
+            key={billingAddress._id}
+          >
+            {`${billingAddress.first_name}
+            ${billingAddress.last_name}`}
+            <br />
+            {`${billingAddress.address}`}
+            <Pen className="absolute top-4 right-4" />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BillingAddressList;
