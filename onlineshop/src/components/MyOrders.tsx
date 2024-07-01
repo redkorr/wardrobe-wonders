f46256233890b0e3c724fddd31630bdb29080521
redkorr <redkor910.97@gmail.com>
2024-07-01 14:00:16 +0200

    feat: created MyOrders component

--- onlineshop/src/components/MyOrders.tsx
+++ onlineshop/src/components/MyOrders.tsx
@@ -1,5 +1,32 @@
+import useOrder from '@/hooks/useOrder';
+import { useUser } from '@clerk/clerk-react';
+import { useEffect } from 'react';
+import { OrderButton } from '.';
+
 const MyOrders = () => {
-  return <div></div>;
+  const { getOrderById, data: orders } = useOrder();
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (user) {
+      getOrderById(user.id);
+    }
+  }, [user]);
+
+  return (
+    <div>
+      {Array.isArray(orders) && (
+        <>
+          {orders?.map((order) => (
+            <OrderButton
+              key={order.order_id}
+              order={order}
+            />
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default MyOrders;
