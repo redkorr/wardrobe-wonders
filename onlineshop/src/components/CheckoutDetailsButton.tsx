1021ae5f5499b63a71e50ecc4b1ba92067aad1ba
redkor <redkor910.97@gmail.com>
2024-09-17 15:32:33 +0200

    feat: created CheckoutDetailsButton component

---
+++ onlineshop/src/components/CheckoutDetailsButton.tsx
@@ -0,0 +1,27 @@
+import usePayment from '@/hooks/usePayment';
+
+interface CheckoutDetailsButtonProps {
+  buttonText: string;
+  orderId: string;
+}
+
+const CheckoutDetailsButton = ({ buttonText, orderId }: CheckoutDetailsButtonProps) => {
+  const { getClientSecret } = usePayment(orderId);
+  return (
+    <div className="w-full">
+      <button
+        form="checkout-form"
+        type="submit"
+        className="w-full py-2 px-3 text-center border border-blue-500 bg-blue-900 text-slate-100"
+        onClick={async () => {
+          const response = await getClientSecret();
+          window.location.href = response.session.url;
+        }}
+      >
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default CheckoutDetailsButton;
