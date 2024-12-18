d4710d4f49a3f5ef84c20b41976fe6ac10abc804
redkor <redkor910.97@gmail.com>
2024-12-18 14:14:21 +0100

    feat: added new type

--- server/mockdata/categories.ts
+++ server/mockdata/categories.ts
@@ -5,24 +5,25 @@ export const categories: Array<Category> = [
   {
     name: 'cloth',
     types: [
-      new Types.ObjectId('653a64ff0de8e3dc995d7691'),
-      new Types.ObjectId('653a64ff0de8e3dc995d7692'),
-      new Types.ObjectId('653a64ff0de8e3dc995d7693'),
-      new Types.ObjectId('653a64ff0de8e3dc995d7694'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40012'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40013'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40014'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40015'),
     ],
   },
   {
     name: 'shoes',
     types: [
-      new Types.ObjectId('653a64ff0de8e3dc995d7695'),
-      new Types.ObjectId('653a64ff0de8e3dc995d7696'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40016'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40017'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e4001a'),
     ],
   },
   {
     name: 'accessories',
     types: [
-      new Types.ObjectId('653a64ff0de8e3dc995d7697'),
-      new Types.ObjectId('653a64ff0de8e3dc995d7698'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40018'),
+      new Types.ObjectId('6762ba88b6b04cd8c6e40019'),
     ],
   },
 ];
