b647e7843ed7948b3ea0adf5eaf6296806f5f01b
redkor <redkor910.97@gmail.com>
2024-12-19 19:36:55 +0100

    feat: updated selectFilters function to check for color filter

--- onlineshop/src/utils/selectFilters.ts
+++ onlineshop/src/utils/selectFilters.ts
@@ -3,8 +3,14 @@ import { FilterState } from 'types';
 const selectFilters = (selectedFilters: FilterState, filterKey: string) => {
   const arrayOfSelectedFilters: Array<string> = [];
   Object.keys(selectedFilters[filterKey as keyof typeof selectedFilters]).forEach((key) => {
-    if (selectedFilters[filterKey as keyof typeof selectedFilters][key]) {
-      arrayOfSelectedFilters.push(key);
+    if (filterKey === 'colors') {
+      if (selectedFilters[filterKey as keyof typeof selectedFilters][key].isTrue) {
+        arrayOfSelectedFilters.push(key);
+      }
+    } else {
+      if (selectedFilters[filterKey as keyof typeof selectedFilters][key]) {
+        arrayOfSelectedFilters.push(key);
+      }
     }
   });
   return arrayOfSelectedFilters;
