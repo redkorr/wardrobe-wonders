7ad478aac3bea9c458a4407c31ba8b9b8e0915d8
redkor <redkor910.97@gmail.com>
2024-12-20 15:05:31 +0100

    fix: fixed fliters

--- onlineshop/src/components/ProductsFilters.tsx
+++ onlineshop/src/components/ProductsFilters.tsx
@@ -5,16 +5,17 @@ import { useState } from 'react';
 interface ProductsFiltersProps {
   category: string | undefined;
   type: string | undefined;
+  sex: string | undefined;
 }
 
 type DropdownsStateTuple = [boolean, boolean, boolean];
 
 const DROPDOWNS_TUPLE_INITIAL_STATE: DropdownsStateTuple = [false, false, false];
 
-const ProductsFilters = ({ category, type }: ProductsFiltersProps) => {
+const ProductsFilters = ({ category, type, sex }: ProductsFiltersProps) => {
   const [dropdownStates, setDropdownStates] = useState<DropdownsStateTuple>(DROPDOWNS_TUPLE_INITIAL_STATE);
 
-  const filters = useFilters(category, type);
+  const filters = useFilters(category, type, sex);
 
   const handleDropdownClick = (index: number) => {
     const newStates = dropdownStates.map((state, i) => {
