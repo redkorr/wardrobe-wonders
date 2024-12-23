70a772473a4eea47c8edc371396babaa5f88f74f
redkor <redkor910.97@gmail.com>
2024-12-23 16:36:38 +0100

    fix: removed unnecessary prop

--- onlineshop/src/pages/ShoppingCart.tsx
+++ onlineshop/src/pages/ShoppingCart.tsx
@@ -5,10 +5,7 @@ const ShoppingCart = () => {
     <>
       <div className=" flex m-8 mt-32 gap-8 font-medium">
         <ShoppingCartList />
-        <CheckoutDetails
-          buttonText="Go to the checkout"
-          path="checkout"
-        />
+        <CheckoutDetails buttonText="Go to the checkout" />
       </div>
     </>
   );
