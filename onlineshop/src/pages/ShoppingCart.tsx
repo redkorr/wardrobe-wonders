fdc049abf058bd783cd4221d1b2a0803f47f5be5
redkor <redkor910.97@gmail.com>
2025-01-23 11:32:02 +0100

    fix: fixed footer

--- onlineshop/src/pages/ShoppingCart.tsx
+++ onlineshop/src/pages/ShoppingCart.tsx
@@ -3,7 +3,7 @@ import { CheckoutDetails, ShoppingCartList } from '@/components';
 const ShoppingCart = () => {
   return (
     <>
-      <div className=" flex m-8 mt-32 gap-8 font-medium">
+      <div className=" flex mx-8 mt-[94px] mb-[95px] pt-20 gap-8 font-medium">
         <ShoppingCartList />
         <CheckoutDetails buttonText="Go to the checkout" />
       </div>
